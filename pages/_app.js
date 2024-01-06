1f1b0bd038be3318c006d899625a6c73dd61b650
Ronit <ronitvirwani1@gmail.com>
2024-01-06 21:36:30 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import Header from '@/components/header'
+import Header from '@/components/Header'
 import GlobalState from '@/context'
 
 export default function App({ Component, pageProps }) {
